22b4d1e626f88628c3e074b1e964c4c4ca32e338
ntbloom <ntbloom@gmail.com>
2021-09-25 11:26:27 -0400

    made cleaner

--- patterns/by_type.py
+++ patterns/by_type.py
@@ -1,34 +1,29 @@
-def show_number(num: any, reference: int):
-    match num:
+def show_number(val: any, reference: int):
+    match val:
         case float():
-            print(f"{num} is a float")
+            print(f"{val} is a float")
         case str():
-            print(f"{num} is a string")
-        case x if type(num) is not int:
-            print(f"{num} is a type `{type(x).__name__}`")
-        case _ if num < reference:
-            print(f"{num} is less than {reference}")
+            print(f"`{val}` is a string")
+        case x if type(val) is not int:
+            print(f"{val} is a type `{type(x).__name__}`")
+        case _ if val < 0:
+            print(f"{val} is negative")
+        case _ if val < reference:
+            print(f"{val} is less than {reference}")
         case 10:
-            print(f"{num} is the same as {reference}")
-        case 11 | 12:
-            print(f"{num} is a little bigger than {reference}")
-        case _ if num == reference * 2:
-            print(f"{num} is twice the size of {reference}")
+            print(f"{val} is the same as {reference}")
+        case 11 | 12 | 13:
+            print(f"{val} is a little bigger than {reference}")
+        case _ if val == reference * 2:
+            print(f"{val} is twice the size of {reference}")
         case _:
-            print(f"{num} is bigger than {reference}")
+            print(f"{val} is bigger than {reference}")
 
 
 if __name__ == "__main__":
     num = 10
     for i in range(12):
         res = num + i
-        if i == 3:
-            res = {"hello": "world"}
-        if i == 5:
-            res = 3.14
-        if i == 6:
-            res = "hello, world"
-        if i == 7:
-            res = 9
         show_number(res, num)
-        print()
+    for others in [-9, -10, 5, {"hello": "world"},3.14, "hello world"]:
+        show_number(others,num)
